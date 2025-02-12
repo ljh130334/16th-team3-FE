92137d1dc950673ef510500caed4370a68174993
prgmr99 <johnyeom24@naver.com>
2025-02-12 21:32:25 +0900

    SCRUM-67 feat: 인가 코드 발급 / SDK 로드된 후 로직 실행되도록 개선

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,50 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 declare global {
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    Kakao: any;
+    Kakao?: any;
   }
 }
 
+const redirectUri = 'http://localhost:3000/oauth/callback/kakao';
+const scope = ['openid'].join(',');
+
 const LoginPage = () => {
-  useEffect(() => {
-    if (window.Kakao && !window.Kakao.isInitialized()) {
-      window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+  const [isKakaoLoaded, setIsKakaoLoaded] = useState(false);
+
+  const kakaoLoginHandler = () => {
+    if (!isKakaoLoaded || !window.Kakao?.Auth) {
+      console.error('Kakao SDK not loaded');
+      return;
     }
+
+    window.Kakao.Auth.authorize({
+      redirectUri,
+      scope,
+    });
+  };
+
+  useEffect(() => {
+    const checkKakaoSDK = () => {
+      if (window.Kakao) {
+        if (!window.Kakao.isInitialized()) {
+          window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+        }
+        setIsKakaoLoaded(true);
+      }
+    };
+
+    checkKakaoSDK();
   }, []);
 
   return (
     <div>
-      <button>카카오 로그인</button>
+      <button onClick={kakaoLoginHandler} disabled={!isKakaoLoaded}>
+        카카오 로그인
+      </button>
     </div>
   );
 };
