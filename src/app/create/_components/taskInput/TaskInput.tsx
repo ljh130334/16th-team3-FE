d35a15644e45f722304803bc4e9830d2a6340f16
prgmr99 <johnyeom24@naver.com>
2025-02-17 21:29:30 +0900

    SCRUM-81 feat: 유효성 검사 문구 추가

--- src/app/create/_components/taskInput/TaskInput.tsx
+++ src/app/create/_components/taskInput/TaskInput.tsx
@@ -2,6 +2,7 @@ import ClearableInput from '@/components/clearableInput/ClearableInput';
 import { useEffect, useRef, useState } from 'react';
 
 const WAITING_TIME = 300;
+const MAX_TASK_LENGTH = 15;
 
 const TaskInput = () => {
   const [task, setTask] = useState<string>('');
@@ -23,6 +24,23 @@ const TaskInput = () => {
   return (
     <div>
       <ClearableInput ref={inputRef} value={task} onChange={handleTaskChange} />
+
+      {task.length > MAX_TASK_LENGTH && (
+        <p className="text-sm text-red-500">
+          최대 16자 이내로 입력할 수 있어요.
+        </p>
+      )}
+
+      <button
+        className={`mt-5 w-full rounded-md px-4 py-2 font-semibold text-white transition ${
+          task.length > MAX_TASK_LENGTH
+            ? 'cursor-not-allowed bg-gray-400'
+            : 'bg-blue-500 hover:bg-blue-600'
+        }`}
+        disabled={task.length > MAX_TASK_LENGTH}
+      >
+        다음
+      </button>
     </div>
   );
 };
