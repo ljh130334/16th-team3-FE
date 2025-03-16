b6168e56b382b17b44149690637d3206f7a9565e
prgmr99 <johnyeom24@naver.com>
2025-03-16 18:30:13 +0900

    chore: add AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Providers from './providers';
 import Script from 'next/script';
-import { AuthProvider } from '@/hooks/useAuth';
+import AuthProvider from './testProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
---
+++ src/app/testProvider.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { api } from '@/lib/ky';
+import { useUserStore } from '@/store';
+import { User } from '@/types/user';
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export default function AuthProvider({ children }: AuthProviderProps) {
+  const setUser = useUserStore((state) => state.setUser);
+
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const res = await api.get('/v1/members/me');
+        if (!res.ok) {
+          setUser({});
+          return;
+        }
+        const data = await res.json<User>();
+        setUser(data);
+      } catch (error) {
+        console.error('API 호출 중 오류 발생:', error);
+        setUser({});
+      }
+    }
+
+    fetchUser();
+  }, [setUser]);
+
+  return <>{children}</>;
+}
