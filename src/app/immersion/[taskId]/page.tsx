c396bcdaef9e6121ecadaa2cff5192ac5e79e44e
정현우 <nolang3748@naver.com>
2025-03-17 17:55:56 +0900

    fix: 코드 원복

--- src/app/immersion/[taskId]/page.tsx
+++ src/app/immersion/[taskId]/page.tsx
@@ -1,53 +1,26 @@
-// app/Immersion/page.tsx (Server Component)
 import { fetchTask } from '@/lib/task';
+import { TaskResponse } from '@/types/task';
 import { cookies } from 'next/headers';
+
 import ImmersionPageClient from './ImmersionPageClient';
 import { CurrentTimeProvider } from '@/provider/CurrentTimeProvider';
 
 export default async function Immersion({
   params,
 }: {
-  params: { taskId: string };
+  params: Promise<{ taskId: string }>;
 }) {
   const { taskId } = await params;
-
-  // 쿠키 확인
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const accessToken = cookieStore.get('accessToken')?.value;
-
+  // TODO(supersett) : 토큰이 없을때 로그인화면으로 가게 하는 로직 추가
   if (!accessToken) {
-    return (
-      <CurrentTimeProvider>
-        <ImmersionPageClient
-          initialTask={null}
-          errorMessage="Access token is not found"
-        />
-      </CurrentTimeProvider>
-    );
+    throw new Error('Access token is not found');
   }
-
-  // 토큰이 있는 경우
-  const task = await fetchTask(taskId, accessToken).catch((error) => {
-    // 예: API 호출 실패 시 에러 메시지 전달
-    return null; // 혹은 다른 방식으로 처리
-  });
-
-  // task가 null이면 API 실패로 가정
-  if (!task) {
-    return (
-      <CurrentTimeProvider>
-        <ImmersionPageClient
-          initialTask={null}
-          errorMessage="Failed to fetch task data."
-        />
-      </CurrentTimeProvider>
-    );
-  }
-
-  // 정상적으로 데이터를 받았을 경우
+  const task: TaskResponse = await fetchTask(taskId, accessToken);
   return (
     <CurrentTimeProvider>
-      <ImmersionPageClient initialTask={task} errorMessage="" />
+      <ImmersionPageClient initialTask={task} />
     </CurrentTimeProvider>
   );
 }
