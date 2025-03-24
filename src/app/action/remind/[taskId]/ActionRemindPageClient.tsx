0f6d7d0e1540e5f17ffc9c8a1cc317f004c9738d
정현우 <nolang3748@naver.com>
2025-03-24 17:51:16 +0900

    feat: 코드 수정

--- src/app/action/remind/[taskId]/ActionRemindPageClient.tsx
+++ src/app/action/remind/[taskId]/ActionRemindPageClient.tsx
@@ -66,14 +66,7 @@ const useReminderCount = (initialCount: number = DEFAULT_VALUES.COUNT) => {
 };
 
 function formatTimestamp(timestamp: string): string {
-  const date = new Date(timestamp);
-  const year = date.getUTCFullYear();
-  const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
-  const day = date.getUTCDate().toString().padStart(2, '0');
-  const hours = date.getUTCHours().toString().padStart(2, '0');
-  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
-  const seconds = date.getUTCSeconds().toString().padStart(2, '0');
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  return timestamp.replace('T', ' ');
 }
 
 export default function ActionRemindPageClient({
