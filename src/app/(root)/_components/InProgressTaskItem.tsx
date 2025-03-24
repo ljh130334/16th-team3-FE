30cfa1ce8d529b7c1912b60258e0202910503b56
fr0gydev <ljh130334@naver.com>
2025-03-24 14:25:08 +0900

    fix: 1시간 내 급한 일 background blur 처리

--- src/app/(root)/_components/InProgressTaskItem.tsx
+++ src/app/(root)/_components/InProgressTaskItem.tsx
@@ -250,12 +250,21 @@ const InProgressTaskItem: React.FC<InProgressTaskItemProps> = ({
 					<p className="b3 text-text-neutral">{formatDueTime()}</p>
 				</div>
 
-				<div className="my-8 flex justify-center overflow-hidden">
+				<div className="my-8 flex justify-center items-center relative">
+					<div
+						className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[103px] h-[105px] mix-blend-color-dodge"
+						style={{
+							background:
+								"conic-gradient(from 140deg at 50.42% 51.28%, rgba(182, 208, 247, 0.70) 0deg, rgba(238, 244, 225, 0.70) 82.50000178813934deg, rgba(226, 193, 249, 0.70) 155.6249964237213deg, rgba(191, 209, 249, 0.70) 209.30935621261597deg, rgba(250, 252, 254, 0.70) 252.6490044593811deg, rgba(186, 228, 226, 0.70) 288.7499928474426deg, rgba(140, 107, 227, 0.70) 341.2500071525574deg, rgba(192, 215, 243, 0.70) 360deg)",
+							filter: "blur(37px)",
+						}}
+					/>
 					<Image
 						src="/icons/home/happy-character.png"
 						alt="Character"
 						width={87}
 						height={87}
+						className="relative z-10"
 					/>
 				</div>
 
