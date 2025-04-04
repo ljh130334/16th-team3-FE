4e255a0a38e072baa1eb152f4f40f63c8c84cd79
정현우 <nolang3748@naver.com>
2025-04-05 01:53:05 +0900

    feat: 작은행동 완료 페이지 작업

--- src/app/(protected)/action/complete/page.tsx
+++ src/app/(protected)/action/complete/page.tsx
@@ -3,22 +3,45 @@
 import { useTaskProgressStore } from '@/store/useTaskStore';
 import { formatKoreanDateTime } from '@/utils/dateFormat';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useUserStore } from '@/store/useUserStore';
 
 import { Badge } from '@/components/component/Badge';
 import CharacterMotivation from './_component/CharacterMotivation';
 import Header from './_component/Header';
 import PhotoCard from './_component/PhotoCard';
 import StartButton from './_component/StartButton';
+import { getPersonaImage } from '@/utils/getPersonaImage';
+import { api } from '@/lib/ky';
+import { User } from '@/types/user';
 
 export default function Complete() {
+  const userData = useUserStore((state) => state.userData);
   const [capturedImage, setCapturedImage] = useState<string>('');
   const currentTask = useTaskProgressStore((state) => state.currentTask);
+  const personaImageSrc = getPersonaImage(currentTask?.persona.id);
+  const setUser = useUserStore((state) => state.setUser);
+
+  const loadUserProfile = useCallback(async () => {
+    const response = await api.get('v1/members/me').json<User>();
+    setUser(response);
+  }, [setUser]);
 
   useEffect(() => {
     setCapturedImage(localStorage.getItem('capturedImage') || '');
     console.log('capturedImage', capturedImage);
   }, []);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await loadUserProfile();
+      } catch (error) {
+        console.error('인증 확인 중 오류:', error);
+      }
+    };
+    checkAuth();
+  }, [loadUserProfile]);
 
   return (
     <div className="flex h-full w-full flex-col gap-4 bg-background-primary">
@@ -37,11 +60,17 @@ export default function Complete() {
 
         <div className="relative flex h-[245px] flex-col items-center justify-center gap-[27px]">
           <Image
-            src="/icons/immersion/study.png"
-            alt="모래시계"
-            width={160}
-            height={160}
+            src={personaImageSrc}
+            alt="페르소나 이미지"
+            width={165}
+            height={165}
           />
+
+          <div className="relative flex h-[26px] items-center justify-center overflow-hidden rounded-[8px] px-[7px] py-[6px] text-black before:absolute before:inset-0 before:-z-10 before:bg-[conic-gradient(from_220deg_at_50%_50%,_#F2F0E7_0%,_#BBBBF1_14%,_#B8E2FB_24%,_#F2EFE8_37%,_#CCE4FF_48%,_#BBBBF1_62%,_#C7EDEB_72%,_#E7F5EB_83%,_#F2F0E7_91%,_#F2F0E7_100%)] before:[transform:scale(4,1)]">
+            <span className="l6 text-inverse">
+              {`${currentTask?.persona.name} ${userData.nickname}`}
+            </span>
+          </div>
         </div>
       </div>
       <StartButton currentTaskId={currentTask?.id?.toString() ?? ''} />
