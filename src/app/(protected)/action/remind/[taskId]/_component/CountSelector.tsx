1ddd619a7ff69d113d2242ca5f8a4befc6a0a630
skydreamer <skydreamer210@gmail.com>
2025-04-05 05:52:03 +0900

    fix: 리마인더 - 알림 카운트 스타일 수정

--- src/app/(protected)/action/remind/[taskId]/_component/CountSelector.tsx
+++ src/app/(protected)/action/remind/[taskId]/_component/CountSelector.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 interface ReminderCountSelectorProps {
   count: number;
   onIncrease: () => void;
@@ -18,19 +20,29 @@ export default function ReminderCountSelector({
       <p>다음 리마인더</p>
       <div className="flex h-8 w-[96px] items-center">
         <div
-          className={`flex h-full w-1/3 cursor-pointer items-center justify-center rounded-[8px] bg-gray-400 text-center text-base leading-[145%] text-gray-normal ${count === min || count === 0 ? '!bg-component-gray-secondary' : ''}`}
+          className={`flex h-full w-1/3 cursor-pointer items-center justify-center rounded-[8px] text-center text-base leading-[145%] text-gray-normal bg-component-gray-secondary ${count === min || count === 0 ? 'opacity-40' : ''}`}
           onClick={onDecrease}
         >
-          -
+          <Image
+            src="/icons/remind/minus.svg"
+            alt="왼쪽 화살표"
+            width={16}
+            height={16}
+          />
         </div>
         <div className="flex h-full w-1/3 items-center justify-center rounded-[8px] text-center text-s2 text-gray-normal">
           {count}
         </div>
         <div
-          className={`flex h-full w-1/3 cursor-pointer items-center justify-center rounded-[8px] bg-gray-400 text-center text-base leading-[145%] text-gray-normal ${count === max ? '!bg-component-gray-secondary' : ''}`}
+          className={`flex h-full w-1/3 cursor-pointer items-center justify-center rounded-[8px] text-center text-base leading-[145%] text-gray-normal bg-component-gray-secondary  ${count === max ? 'opacity-40' : ''}`}
           onClick={onIncrease}
         >
-          +
+          <Image
+            src="/icons/remind/plus.svg"
+            alt="왼쪽 화살표"
+            width={16}
+            height={16}
+          />
         </div>
       </div>
     </div>
