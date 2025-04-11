15fcd66a93140e1ad9516cf70d73b56e7194c23d
fr0gydev <ljh130334@naver.com>
2025-04-12 03:16:56 +0900

    fix: 회고 종료 화면 몰입 종료 화면과 동일하게 수정

--- src/app/(protected)/retrospection/complete/page.tsx
+++ src/app/(protected)/retrospection/complete/page.tsx
@@ -4,22 +4,20 @@ import Link from "next/link";
 
 export default function RetrospectionCompletePage() {
 	return (
-		<div className="flex h-full flex-col gap-4 bg-background-primary">
-			<div className="mt-[180px] flex flex-col items-center gap-2">
-				<div className="h-[180px] flex items-center justify-center">
-					<Image
-						src="/retro-complete.svg"
-						alt="logo"
-						width={142}
-						height={80}
-						priority
-					/>
-				</div>
+		<div className="flex h-full flex-col bg-background-primary">
+			<div className="mt-[180px] flex flex-col items-center">
+				<Image
+					src="/retro-complete.svg"
+					alt="logo"
+					width={142}
+					height={68}
+					className="mb-10"
+				/>
 
-				<div className="flex flex-col gap-1 text-center">
-					<p className="text-t3">회고 작성 완료!</p>
-					<p className="text-t3">오늘의 몰입 멋지게 정리했어요</p>
-				</div>
+				<p className="text-t3 text-gray-strong">회고 작성 완료!</p>
+				<p className="text-t3 text-gray-strong mb-2">
+					오늘도 몰입 멋지게 정리했어요
+				</p>
 			</div>
 			<div className="relative mt-auto flex flex-col items-center px-5 py-6">
 				<Link href="/" className="w-full">
