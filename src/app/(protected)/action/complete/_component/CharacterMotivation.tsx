1b082980741515dd0114d2b7ca683db1a441f443
skydreamer <skydreamer210@gmail.com>
2025-04-19 14:38:28 +0900

    fix: flex 수정

--- src/app/(protected)/action/complete/_component/CharacterMotivation.tsx
+++ src/app/(protected)/action/complete/_component/CharacterMotivation.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function CharacterMotivation({ currentTask, nickname }: Props) {
 	return (
-		<div className="mt-[30px] flex items-center justify-center text-center text-t3 font-semibold text-gray-strong">
+		<div className="mt-[30px] flex flex-col items-center justify-center text-center text-t3 font-semibold text-gray-strong">
 			잘했어요! 이제
 			<br />
 			<span className="text-component-accent-secondary">
